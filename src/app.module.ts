f8fdaa9dac2d6476b4127a033a4183c7bdc28d49
Francisco Guilherme <guilhermeee314@gmail.com>
2024-04-24 12:14:09 -0300

    Subindo BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ProdutoModule } from './produto/produto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgreeConfigService } from 'config/postgree.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [UsuarioModule, ProdutoModule],
+  imports: [UsuarioModule, 
+    ProdutoModule, 
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    
+    TypeOrmModule.forRootAsync({
+    useClass: PostgreeConfigService,
+    inject: [PostgreeConfigService]
+  })],
 
 })
 export class AppModule {}
